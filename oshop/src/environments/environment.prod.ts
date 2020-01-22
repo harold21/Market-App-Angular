e12e722cc89c99f0fe8f412d0220375b41dfaef1
Harold <harold.buenaventura@nanovision.com>
2020-01-22 11:10:01 -0300

    Initial Project File
    First commint and create the main nav-bar

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAiPEQ3KjeGTeuoKNFuwsRLxx_C6HtEG_Q',
+    authDomain: 'oshop-c05a2.firebaseapp.com',
+    databaseURL: 'https://oshop-c05a2.firebaseio.com',
+    projectId: 'oshop-c05a2',
+    storageBucket: 'oshop-c05a2.appspot.com',
+    messagingSenderId: '633110705456',
+    appId: '1:633110705456:web:3f4f15e96c1e81e43ac4f5',
+    measurementId: 'G-7D3G1868ES'
+  }
+};
